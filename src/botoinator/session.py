edd225abf8fa207105e739001e85d2cd3e28fa89
Mathew Moon <mmoon@athena-iot.com>
2019-08-20 11:21:32 -0500

    Make variable names more intuitive

--- src/botoinator/session.py
+++ src/botoinator/session.py
@@ -5,8 +5,8 @@ class DecoratedSession(boto3.session.Session):
 
 
   __decorators = {}
-  
-  
+
+
   def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  aws_session_token=None, region_name=None,
                  botocore_session=None, profile_name=None):
@@ -21,23 +21,23 @@ class DecoratedSession(boto3.session.Session):
     self.__decorators = {}
     for event_name, decorator_map in DecoratedSession.__decorators.items():
       for method_name, decorator in decorator_map.items():
-        self.__register_decorator(event_name, method_name, decorator) 
-    
+        self.__register_decorator(event_name, method_name, decorator)
+
 
   @classmethod
-  def add_client_decorator(cls, client_name, method_names, decorator):
+  def add_client_decorator(cls, service_name, method_names, decorator):
     """
     Add the decorator function to the class' statically registered decorators.
 
     Class-registered decorators will be applied to every DecoratedSession object created.
 
     Arguments:
-    client_name -- the boto3 name of the client to apply the decorator to.
+    client_name -- the boto3 name of the service to apply the decorator to.
     method_names -- one or more method names of the client to apply the decorator to. Single names can be a string.
     decorator -- the decorator function. Must be a function that takes a function and returns a function. The returned function must take (*args, **kwargs) as arguments.
     """
     assert isinstance(method_names, (str, list, tuple, set)), 'method_names must be a string, list, tuple or set'
-    event_name = 'creating-client-class.{}'.format(client_name)
+    event_name = 'creating-client-class.{}'.format(service_name)
     if isinstance(method_names, str):
       cls.__add_decorator(event_name, method_names, decorator)
     else:
@@ -128,7 +128,7 @@ class DecoratedSession(boto3.session.Session):
       for method_name in method_names:
         assert isinstance(method_name, str), 'method_name {} must be a string'.format(m)
         cls.__remove_decorator(event_name, method_name)
-    
+
 
 
   @classmethod
@@ -152,16 +152,16 @@ class DecoratedSession(boto3.session.Session):
         cls.__remove_decorator(event_name, method_name)
 
 
-  def unregister_client_decorator(self, client_name, method_names):
+  def unregister_client_decorator(self, service_name, method_names):
     """
     Removes the decorator function from the session's registered decorators.
 
     Arguments:
-    client_name -- the boto3 name of the client to apply the decorator to.
+    service_name -- the boto3 name of the client to apply the decorator to.
     method_names -- one or more method names of the client to apply the decorator to. Single names can be a string.
     """
     assert isinstance(method_names, (str, list, tuple, set, None)), 'method_names must be a string, list, tuple, set or None'
-    event_name = 'creating-client-class.{}'.format(client_name)
+    event_name = 'creating-client-class.{}'.format(service_name)
     if not method_names or isinstance(method_names, str):
       self.__unregister_decorator(event_name, method_names)
     else:
@@ -186,7 +186,7 @@ class DecoratedSession(boto3.session.Session):
       for method_name in method_names:
         self.__unregister_decorator(event_name, method_name)
 
-  
+
   @classmethod
   def __add_decorator(cls, event_name, method_name, decorator):
     assert callable(decorator), 'decorator must be a function'
